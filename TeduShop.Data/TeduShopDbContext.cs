ce30192038194cf6fcf99900cb20ffbbb27f482e
Pham Dang Khoa <mr.pdkhoa@gmail.com>
2018-03-15 16:30:43 +0700

    add aspnet identity

--- TeduShop.Data/TeduShopDbContext.cs
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -1,9 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using TeduShop.Model.Models;
 
 namespace TeduShop.Data
 {
-    public class TeduShopDbContext : DbContext
+    public class TeduShopDbContext : IdentityDbContext<ApplicationUser>
     {
         public TeduShopDbContext() : base("TeduShopConnection")
         {
@@ -29,9 +30,15 @@ namespace TeduShop.Data
         public DbSet<VisitorStatistic> VisitorStatistic { set; get; }
         public DbSet<Error> Errors { set; get; }
 
+        public static TeduShopDbContext Create()
+        {
+            return new TeduShopDbContext();
+        }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId });
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(i => i.UserId);
         }
     }
 }
--- TeduShop.Web/Mappings/AutoMapperConfiguration.cs
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -12,10 +12,36 @@ namespace TeduShop.Web.Mappings
     {
         public static void Configure()
         {
-            Mapper.CreateMap<Post, PostViewModel>();
-            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
-            Mapper.CreateMap<Tag, TagViewModel>();
-            Mapper.CreateMap<PostTag, PostTagViewModel>();
+            //Mapper.CreateMap<Post, PostViewModel>();
+            //Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            //Mapper.CreateMap<Tag, TagViewModel>();
+            //Mapper.CreateMap<PostTag, PostTagViewModel>();
+
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.CreateMap<Category, CategoryViewModel>().MaxDepth(2);
+                cfg.CreateMap<Post, PostViewModel>().MaxDepth(2);
+                cfg.CreateMap<Tag, TagViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Ebook, EbookViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Page, PageViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseCategory, CourseCategoryViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Course, CourseViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppGroup, AppGroupViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppRole, AppRoleViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppUser, AppUserViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppUserGroup, AppUserGroupViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Trainer, TrainerViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseVideo, CourseVideoViewModel>().MaxDepth(2);
+                //cfg.CreateMap<TechLine, TechLineViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseUser, CourseUserViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseReview, CourseReviewViewModel>().MaxDepth(2);
+                //cfg.CreateMap<PaymentMethod, PaymentMethodViewModel>().MaxDepth(2);
+                //cfg.CreateMap<TransactionLog, TransactionLogViewModel>().MaxDepth(2);
+                //cfg.CreateMap<VideoComment, VideoCommentViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Notification, NotificationViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Footer, FooterViewModel>().MaxDepth(2);
+
+            });
         }
     }
 }
